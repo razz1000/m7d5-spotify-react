7a14712d53b706d801f68a4bfc8642f5f45da532
razz1000 <76906581+razz1000@users.noreply.github.com>
2022-06-17 09:55:09 +0100

    push

--- src/components/DisplaySelected.jsx
+++ src/components/DisplaySelected.jsx
@@ -1,12 +1,37 @@
 import { Card, Button, Figure, Container, Row, Col } from "react-bootstrap";
+import { connect } from "react-redux";
 
-let DisplaySelected = () => {
+const mapStateToProps = (state) => ({
+  song: state.selected.song,
+});
+
+const mapDispatchToProps = () => ({});
+
+const DisplaySelected = ({ song }) => {
   return (
     <div>
+      {console.log("SONG:", song)}
       <Container style={{ color: "red" }}>
         <Row>
           <Col>
-            <p>Hello there</p>
+            <Figure>
+              <Figure.Image
+                width={160}
+                height={160}
+                alt="171x180"
+                src="http://placekitten.com/160/160"
+              />
+              <Figure.Caption></Figure.Caption>
+            </Figure>
+          </Col>
+          <Col>
+            {/*             <Row>{<p>{song.title}</p>}</Row>
+            <Row>{<p>{song.artist.name}</p>}</Row> */}
+          </Col>
+          <Col>
+            <Row>
+              <i class="bi bi-heart"></i>
+            </Row>
           </Col>
         </Row>
       </Container>
@@ -14,4 +39,4 @@ let DisplaySelected = () => {
   );
 };
 
-export default DisplaySelected;
+export default connect(mapStateToProps, mapDispatchToProps)(DisplaySelected);
